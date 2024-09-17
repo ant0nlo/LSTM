eb8e66dc980adffc339b53b56fc076a6eb368a6f
antonio <antoniopenev23@gmail.com>
2024-09-17 12:27:17 +0300

    Updated README and added LSTM model script

--- lstm.py
+++ lstm.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 #Using the past 60 days stock price
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
@@ -16,9 +13,6 @@ import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
 
-# In[2]:
-
-
 # Load the data from the CSV file
 df = pd.read_csv('AAPL-2.csv')
 
@@ -26,17 +20,11 @@ df = pd.read_csv('AAPL-2.csv')
 print(df)
 
 
-# In[3]:
-
-
 #get rows and columns
 df.shape
 
 
-# In[4]:
-
-
-#visualize yhe closing price history
+#visualize the closing price history
 plt.figure(figsize=(16,8))
 plt.title('Close Price History')
 plt.plot(df['Close'])
@@ -45,19 +33,13 @@ plt.ylabel('Close Prize USD($)', fontsize = 18)
 plt.show()
 
 
-# In[5]:
-
-
-#create  new dataframe with only the 'Close column'
+#create a new dataframe with only the 'Close' column
 data = df.filter(['Close'])
 #convert the dataset to a numpy array
 dataset = data.values
 #get the number of rows to train the model on
 training_data_len = math.ceil(len(dataset)* .8)
 training_data_len
-
-
-# In[6]:
 
 
 #scale the data
@@ -67,14 +49,11 @@ scaled_data = scaler.fit_transform(dataset)
 scaled_data
 
 
-# In[7]:
-
-
 #Create the training data set
-#Create the scaked trining data set
+#Create the scaled training data set
 train_data = scaled_data[0:training_data_len, :]
 
-#Split the data into x_train and y_train data set
+#Split the data into x_train and y_train data sets
 x_train = [] #independent training variables
 y_train = [] #dependent training variables
 
@@ -88,14 +67,8 @@ for i in range(60, len(train_data)):
         print()
 
 
-# In[8]:
-
-
 #Convert x_train and y_train to numpy arrays
 x_train, y_train = np.array(x_train), np.array(y_train)
-
-
-# In[9]:
 
 
 #Reshape the data
@@ -103,10 +76,7 @@ x_train = np.reshape(x_train,(x_train.shape[0], x_train.shape[1], 1))
 x_train.shape
 
 
-# In[10]:
-
-
-#Built the LSTM model
+#Build the LSTM model
 model = Sequential()
 model.add(LSTM(100, return_sequences=True, input_shape =( x_train.shape[1], 1)))
 model.add(LSTM(100, return_sequences=False))
@@ -114,24 +84,15 @@ model.add(Dense(50))
 model.add(Dense(1))
 
 
-# In[11]:
-
-
 #Compile the model
 model.compile(optimizer='adam', loss ='mean_squared_error') 
-#the optimizer is used to improve upon the loss funtcion
-# the loss function is used to measure how well the model did on training
-
-
-# In[12]:
+#the optimizer is used to improve upon the loss function
+#the loss function is used to measure how well the model did on training
 
 
 #Train the model
 model.fit(x_train, y_train, batch_size = 1, epochs = 1)
 # epoch the number of entire iterations 
-
-
-# In[13]:
 
 
 #Create the testing data set
@@ -147,29 +108,17 @@ for i in range(60, len(test_data)):
     x_test.append(test_data[i-60:i, 0])
 
 
-# In[14]:
-
-
 #Convert the data into numpy array
 x_test = np.array(x_test)
 
 
-# In[15]:
-
-
-#Rehsape the data
+#Reshape the data
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 
-# In[16]:
-
-
-#Get the models predicted the price values 
+#Get the model's predicted price values 
 predictions = model.predict(x_test)
 predictions = scaler.inverse_transform(predictions) #unsclaing the values
-
-
-# In[17]:
 
 
 #Getting the root mean squared error (RMSE) 
@@ -177,9 +126,6 @@ predictions = scaler.inverse_transform(predictions) #unsclaing the values
 #the lower value of RMSE indicate a better fit (0 means that the predictions are perfect)
 rmse = np.sqrt(np.mean(predictions - y_test)**2)
 rmse 
-
-
-# In[18]:
 
 
 #Plot the data
@@ -191,25 +137,19 @@ valid['Predictions'] = predictions[:len(valid)]
 plt.figure(figsize=(16,8))
 plt.title('Model')
 plt.xlabel('Data', fontsize = 18)
-plt.ylabel('Close Prise USD ($)', fontsize = 18)
+plt.ylabel('Close Price USD ($)', fontsize = 18)
 plt.plot(train['Close'])
 plt.plot(valid[['Close', 'Predictions']])
 plt.legend(['Train', 'Val', 'Predictions'], loc = 'lower right')
-plt.show
-
-
-# In[19]:
+plt.show()
 
 
 #Show the valid and predicted prices
 valid
 
 
-# In[20]:
-
-
 #Get the quote
-selected_data = pd.read_csv('/kaggle/input/appl-data-2/AAPL-2.csv')
+selected_data = pd.read_csv('AAPL-2.csv')
 
 # Set the start and end dates
 start_date = '2012-01-01'
@@ -246,10 +186,7 @@ pred_price = scaler.inverse_transform(pred_price)
 print(pred_price)
 
 
-# In[21]:
-
-
-selected_data = pd.read_csv('/kaggle/input/appl-data-2/AAPL-2.csv')
+selected_data = pd.read_csv('AAPL-2.csv')
 
 # Set the start and end dates
 start_date = '2024-03-01'
@@ -260,4 +197,3 @@ filtered_data = selected_data[(selected_data['Date'] >= start_date) & (selected_
 
 # Display the 'Close' prices for this period
 print(filtered_data['Close'])
-
